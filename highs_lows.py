bc39219cffa1a77e28e5f704a66d444ed818c89c
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-02-07 15:49:13 +0100

    plotting two data series

---
+++ highs_lows.py
@@ -0,0 +1,36 @@
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+#  Get dates, high and low temperatures from file
+filename = 'sitka_weather_2018_full.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        if not row[8] or not row[2] or not row[9]:
+            continue
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        dates.append(current_date)
+        high = int(row[8])
+        highs.append(high)
+
+        low = int(row[9])
+        lows.append(low)
+
+    # Plot data.
+    fig = plt.figure(dpi=128, figsize=(10,6))
+    plt.plot(dates, highs, c='red')
+    plt.plot(dates, lows, c='blue')
+
+    # Format plot.
+    plt.title("Daily high and low Temperatures - 2018", fontsize=24)
+    plt.xlabel('', fontsize=16)
+    fig.autofmt_xdate()
+    plt.ylabel("Temperature (F)", fontsize=16)
+    plt.tick_params(axis='both', which='major', labelsize=16)
+
+    plt.show()
